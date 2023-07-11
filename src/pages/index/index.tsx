6fda5a6b8490ae25816bba4f91c86bcda74e5d00
JellyLu <634725546@qq.com>
2023-07-12 00:10:27 +0800

    feat: draw tree

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -8,54 +8,53 @@ export default class Index extends Component {
   componentDidMount() {
     const option = {
       title: {
-        text: "某站点用户访问来源",
-        subtext: "纯属虚构",
-        left: "center",
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)",
-      },
-      legend: {
-        orient: "vertical",
-        left: "left",
-        data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"],
+        text: '族谱图',
       },
       series: [
         {
-          name: "访问来源",
-          type: "pie",
-          radius: "55%",
-          center: ["50%", "60%"],
+          type: 'tree',
           data: [
-            { value: 335, name: "直接访问" },
-            { value: 310, name: "邮件营销" },
-            { value: 234, name: "联盟广告" },
-            { value: 135, name: "视频广告" },
-            { value: 1548, name: "搜索引擎" },
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: "rgba(0, 0, 0, 0.5)",
+            {
+              name: '父亲',
+              children: [
+                {
+                  name: '儿子1',
+                  children: [
+                    {
+                      name: '女儿1',
+                      children: [
+
+                      ]
+                    },
+                    {
+                      name: '儿子2',
+                    },
+                  ],
+                },
+                {
+                  name: '儿子2',
+                },
+              ],
             },
-          },
+          ],
+          expandAndCollapse: true,
+          layout: "radial"
         },
       ],
     };
 
-    this.pieChart.refresh(option);
+
+    this.chart.refresh(option);
   }
 
-  pieChart: any;
+  chart: any;
 
-  refPieChart = (node) => (this.pieChart = node);
+  chartRef = (node) => (this.chart = node);
 
   render() {
     return (
-        <View className='pie-chart'>
-          <EChart ref={this.refPieChart} canvasId='pie-chart' />
+        <View className='family-tree'>
+          <EChart ref={this.chartRef} canvasId='pie-chart' />
         </View>
     );
   }
