fd100e84758bc3abd1e938bf0d971f3271836c7e
JellyLu <634725546@qq.com>
2023-07-15 00:02:07 +0800

    feat: draw tree with relationship

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -7,38 +7,56 @@ import "./index.scss";
 export default class Index extends Component {
   componentDidMount() {
     const option = {
-      title: {
-        text: '族谱图',
-      },
       series: [
         {
-          type: 'tree',
+          type: 'graph',
+          layout: 'force',
+          force: {
+            repulsion: 300,
+            layoutAnimation: false,
+            initLayout: 'x',
+            edgeLength: 100,
+          },
+          label: { show: true, padding: 10 },
+          lineStyle: { join: 'round' },
+          itemStyle: { borderWidth: 1,},
           data: [
+            { name: '甲', category: 0 },
+            { name: '甲夫人', category: 2 },
+            { name: '乙', category: 1 },
+            { name: '乙丈夫', category: 3 },
+            { name: '丙', category: 0 },
+          ],
+          links: [
+            { source: '甲', target: '甲夫人' },
+            { source: '甲', target: '乙' },
+            { source: '甲夫人', target: '乙' },
+            { source: '甲', target: '丙' },
+            { source: '甲夫人', target: '丙' },
+            { source: '乙', target: '乙丈夫' },
+          ],
+          categories: [
             {
-              name: '父亲',
-              children: [
-                {
-                  name: '儿子1',
-                  children: [
-                    {
-                      name: '女儿1',
-                      children: [
-
-                      ]
-                    },
-                    {
-                      name: '儿子2',
-                    },
-                  ],
-                },
-                {
-                  name: '儿子2',
-                },
-              ],
+              name: '子',
+              itemStyle: {color: "white"},
+              label: { backgroundColor: "blue"},
+            },
+            {
+              name: '女',
+              itemStyle: {color: "white"},
+              label: { backgroundColor: "green"},
+            },
+            {
+              name: '妻',
+              itemStyle: {color: "white"},
+              label: { backgroundColor: "red"},
+            },
+            {
+              name: '婿',
+              itemStyle: {color: "white"},
+              label: { backgroundColor: "purple"},
             },
           ],
-          expandAndCollapse: true,
-          layout: "radial"
         },
       ],
     };
